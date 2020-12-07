dfc914e6b79612886ea173c3b0478de24c95603c
etztrefis <etztrefishello@gmail.com>
2020-12-07 10:35:02 +0700

    created CRUD default funtions

---
+++ app/controller/admins.controller.js
@@ -0,0 +1,21 @@
+const db = require("../models");
+const Admins = db.admins;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Admin
+exports.create = (req, res) => {};
+
+// Retrieve all Admins from the database.
+exports.findAll = (req, res) => {};
+
+// Find a single Admin with an id
+exports.findOne = (req, res) => {};
+
+// Update a Admin by the id in the request
+exports.update = (req, res) => {};
+
+// Delete a Admin with the specified id in the request
+exports.delete = (req, res) => {};
+
+// Find all availiable Admins
+exports.findAllAvailiable = (req, res) => {};
