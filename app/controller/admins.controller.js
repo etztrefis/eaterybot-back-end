aa8f02eaf714be5e41d09fa21aecfef7a25ba09f
etztrefis <etztrefishello@gmail.com>
2021-02-09 06:07:12 +0300

    new responses from api

--- app/controller/admins.controller.js
+++ app/controller/admins.controller.js
@@ -6,102 +6,111 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Admin
 exports.create = async (req, res) => {
-	const authHeader = req.headers.authorization;
-	if (authHeader) {
-		const token = authHeader.split(" ")[1];
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
 
-		jwt.verify(token, process.env.TOKEN, async (err) => {
-			if (err) {
-				return res.sendStatus(403);
-			} else {
-				await db.admins
-					.findAll({
-						where: { Login: { [Op.eq]: req.params.username } },
-					})
-					.then(async (value) => {
-						if (value == "") {
-							await db.admins
-								.create({
-									Login: req.params.username,
-									Password: req.params.password,
-									Availialbe: true,
-								})
-								.then(async () => {
-									await db.codes.update(
-										{ Available: false },
-										{ where: { Code: req.params.code } }
-									);
-									return res.sendStatus(200);
-								})
-								.catch(() => {
-									return res.sendStatus(404);
-								});
-						} else {
-							return res.json({ message: "Already exists." });
-						}
-					})
-					.catch(() => {
-						return res.sendStatus(404);
-					});
-			}
-		});
-	}
+    jwt.verify(token, process.env.TOKEN, async (err) => {
+      if (err) {
+        return res.status(403).send({ type: "error", message: "Authorization token required" })
+      } else {
+        await db.admins
+          .findAll({
+            where: { Login: { [Op.eq]: req.params.username } },
+          })
+          .then(async (value) => {
+            if (value == "") {
+              await db.admins
+                .create({
+                  Login: req.params.username,
+                  Password: req.params.password,
+                  Availialbe: true,
+                })
+                .then(async () => {
+                  await db.codes.update(
+                    { Available: false },
+                    { where: { Code: req.params.code } }
+                  );
+                  return res.status(200).send({ type: "OK", message: "Successfully created" });
+                })
+                .catch(() => {
+                  return res.status(404).send({ type: "Error", message: "Error while admin creating" });
+                });
+            } else {
+              return res.status(404).send({ message: "Already exists." });
+            }
+          })
+          .catch(() => {
+            return res.status(404).send({ type: "Error", message: "Error while admin searching" });
+          });
+      }
+    });
+  } else {
+    return res.status(403).send({ type: "Error", message: "Authorization token required" })
+  }
 };
 
 // Retrieve all Admins from the database.
 exports.findAll = async (req, res) => {
-	await db.admins.findAll().then((getAdmins) => {
-		res.json(getAdmins);
-	});
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, async (err) => {
+      if (err) {
+        return res.status(403).send({ type: "Error", message: "Authorization token required" })
+      } else {
+        await db.admins.findAll().then((getAdmins) => {
+          res.status(200).send(getAdmins);
+        });
+      }
+    });
+  } else {
+    return res.status(403).send({ type: "Error", message: "Authorization token required" })
+  }
 };
-
 // Find a single Admin with indicated username and password
 exports.findOne = async (req, res) => {
-	await db.admins
-		.findOne({
-			where: {
-				Login: { [Op.eq]: req.params.username },
-				Availiable: { [Op.eq]: true },
-			},
-		})
-		.then((getAdmin) => {
-			if (getAdmin == null) {
-				res.sendStatus(404);
-			} else {
-				const responseBytes = cryptoJS.AES.decrypt(
-					req.params.password,
-					process.env.REACT_APP_CRYPT
-				);
-				const databadeBytes = cryptoJS.AES.decrypt(
-					getAdmin["Password"],
-					process.env.REACT_APP_CRYPT
-				);
+  await db.admins
+    .findOne({
+      where: {
+        Login: { [Op.eq]: req.params.username },
+        Availiable: { [Op.eq]: true },
+      },
+    })
+    .then((getAdmin) => {
+      if (getAdmin == null) {
+        return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+      } else {
+        const responseBytes = cryptoJS.AES.decrypt(
+          req.params.password,
+          process.env.REACT_APP_CRYPT
+        );
+        const databadeBytes = cryptoJS.AES.decrypt(
+          getAdmin["Password"],
+          process.env.REACT_APP_CRYPT
+        );
 
-				const originalResponse = responseBytes.toString(
-					cryptoJS.enc.Utf8
-				);
-				const originalDataBase = databadeBytes.toString(
-					cryptoJS.enc.Utf8
-				);
+        const originalResponse = responseBytes.toString(cryptoJS.enc.Utf8);
+        const originalDataBase = databadeBytes.toString(cryptoJS.enc.Utf8);
 
-				if (originalResponse === originalDataBase) {
-					res.json(Math.floor(Math.random() * 16777215).toString(16));
-				} else {
-					res.sendStatus(404);
-				}
-			}
-		})
-		.catch((e) => {
-			 console.log(e);
-			res.sendStatus(404);
-		});
+        if (originalResponse === originalDataBase) {
+          res.status(200).send(Math.floor(Math.random() * 16777215).toString(16));
+        } else {
+          return res.status(404).send({ type: "Error", message: "Tokens does not match" });
+        }
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+    });
 };
 
 // Update an Admin by the id in the request
-exports.update = (req, res) => {};
+exports.update = (req, res) => { };
 
 // Delete an Admin with the specified id in the request
-exports.delete = (req, res) => {};
+exports.delete = (req, res) => { };
 
 // Find all availiable Admins
-exports.findAllAvailiable = (req, res) => {};
+exports.findAllAvailiable = (req, res) => { };
