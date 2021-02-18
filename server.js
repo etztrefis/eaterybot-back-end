8f8ceb6a1fc4455a1b0a8b37e98b49ea40a599ca
etztrefis <etztrefishello@gmail.com>
2021-02-18 08:40:18 +0300

    dishes routes and findAll composition route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const adminController = require("./app/controller/admins.controller");
 const codesController = require("./app/controller/codes.controller");
 const productsConrtoller = require("./app/controller/products.controller");
+const dishesController = require("./app/controller/dishes.controller");
+const compositionsController = require("./app/controller/compositions.controller");
 
 const app = express();
 
@@ -34,9 +36,20 @@ app.get("/api/codes/:code", codesController.findAll);
 app.get("/api/codes/", codesController.findAllCodes);
 
 app.get("/api/products/", productsConrtoller.findAll);
+app.get("/api/products/less/", productsConrtoller.findAllLess);
 app.get("/api/products/create/:name/:amount/:units", productsConrtoller.create);
 app.get("/api/products/delete/:id", productsConrtoller.delete);
 app.get("/api/products/update/:id/:name/:amount/:units", productsConrtoller.update);
+
+app.get("/api/dishes/", dishesController.findAll);
+app.get("/api/dishes/create/:id/:name/:energy/:price", dishesController.create);
+app.get("/api/dishes/delete/:id", dishesController.delete);
+app.get("/api/dishes/update/:id/:name/:energy/:price", dishesController.update);
+
+app.get("/api/compositions/", compositionsController.findAll);
+// app.get("/api/compositions/create/:did/:pid/:amount/", compositionsController.create);
+// app.get("/api/compositions/delete/:did/:pid", compositionsController.delete);
+// app.get("/api/compositions/update/:did/:pid/:amount/", compositionsController.update);
 
 const db = require("./app/models");
 db.sequelize.sync().then(() => {
