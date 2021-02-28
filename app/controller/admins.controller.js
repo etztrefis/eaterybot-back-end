6b5792d34d9e10b20bf20ab4acbe618157c933e9
etztrefis <etztrefishello@gmail.com>
2021-02-28 10:45:15 +0300

    functions for update users password and username

--- app/controller/admins.controller.js
+++ app/controller/admins.controller.js
@@ -69,48 +69,60 @@ exports.findAll = async (req, res) => {
 };
 // Find a single Admin with indicated username and password
 exports.findOne = async (req, res) => {
-  await db.admins
-    .findOne({
-      where: {
-        Login: { [Op.eq]: req.params.username },
-        Availiable: { [Op.eq]: true },
-      },
-    })
-    .then((getAdmin) => {
-      if (getAdmin == null) {
-        return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, async (err) => {
+      if (err) {
+        return res.status(403).send({ type: "Error", message: "Authorization token required" })
       } else {
-        const responseBytes = cryptoJS.AES.decrypt(
-          req.params.password,
-          process.env.REACT_APP_CRYPT
-        );
-        const databadeBytes = cryptoJS.AES.decrypt(
-          getAdmin["Password"],
-          process.env.REACT_APP_CRYPT
-        );
+        await db.admins
+          .findOne({
+            where: {
+              Login: { [Op.eq]: req.params.username },
+              Availiable: { [Op.eq]: true },
+            },
+          })
+          .then((getAdmin) => {
+            if (getAdmin == null) {
+              return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+            } else {
+              const responseBytes = cryptoJS.AES.decrypt(
+                req.params.password,
+                process.env.REACT_APP_CRYPT
+              );
+              const databadeBytes = cryptoJS.AES.decrypt(
+                getAdmin["Password"],
+                process.env.REACT_APP_CRYPT
+              );
 
-        const originalResponse = responseBytes.toString(cryptoJS.enc.Utf8);
-        const originalDataBase = databadeBytes.toString(cryptoJS.enc.Utf8);
+              const originalResponse = responseBytes.toString(cryptoJS.enc.Utf8);
+              const originalDataBase = databadeBytes.toString(cryptoJS.enc.Utf8);
 
-        if (getAdmin.isSuperAdmin == 1) {
-          if (originalResponse === originalDataBase) {
-            res.status(200).send(`${Math.floor(Math.random() * 16777215).toString(16)}1`);
-          } else {
-            return res.status(404).send({ type: "Error", message: "Tokens does not match" });
-          }
-        } else {
-          if (originalResponse === originalDataBase) {
-            res.status(200).send(`${Math.floor(Math.random() * 16777215).toString(16)}0`);
-          } else {
-            return res.status(404).send({ type: "Error", message: "Tokens does not match" });
-          }
-        }
+              if (getAdmin.isSuperAdmin == 1) {
+                if (originalResponse === originalDataBase) {
+                  res.status(200).send(`${Math.floor(Math.random() * 16777215).toString(16)} ${getAdmin["Login"]}1`);
+                } else {
+                  return res.status(404).send({ type: "Error", message: "Tokens does not match" });
+                }
+              } else {
+                if (originalResponse === originalDataBase) {
+                  res.status(200).send(`${Math.floor(Math.random() * 16777215).toString(16)} ${getAdmin["Login"]}0`);
+                } else {
+                  return res.status(404).send({ type: "Error", message: "Tokens does not match" });
+                }
+              }
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+            return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+          });
       }
-    })
-    .catch((e) => {
-      console.log(e);
-      return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
     });
+  } else {
+    return res.status(403).send({ type: "Error", message: "Authorization token required" })
+  }
 };
 
 // Delete an Admin with the specified id in the request
@@ -153,6 +165,106 @@ exports.findAllAvailiable = (req, res) => {
         await db.admins.findAll({ where: { Availiable: { [Op.eq]: 1 } } }).then((getAdmins) => {
           res.status(200).send(getAdmins);
         });
+      }
+    });
+  } else {
+    return res.status(403).send({ type: "Error", message: "Authorization token required" })
+  }
+};
+
+// Update users password by current password
+exports.updatePassword = (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, async (err) => {
+      if (err) {
+        return res.status(403).send({ type: "Error", message: "Authorization token required" })
+      } else {
+        await db.admins
+          .findOne({
+            where: {
+              Login: { [Op.eq]: req.params.username },
+              Availiable: { [Op.eq]: true },
+            },
+          })
+          .then((getAdmin) => {
+            if (getAdmin == null) {
+              return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+            } else {
+              const responseBytes = cryptoJS.AES.decrypt(
+                req.params.oldpassword,
+                process.env.REACT_APP_CRYPT
+              );
+              const databadeBytes = cryptoJS.AES.decrypt(
+                getAdmin["Password"],
+                process.env.REACT_APP_CRYPT
+              );
+
+              const originalResponse = responseBytes.toString(cryptoJS.enc.Utf8);
+              const originalDataBase = databadeBytes.toString(cryptoJS.enc.Utf8);
+
+              if (originalResponse === originalDataBase) {
+                getAdmin.update({
+                  Password: req.params.newpassword
+                })
+                  .then(() => {
+                    res.status(200).send({ type: "OK", message: "Successfully updated" })
+                  })
+                  .catch(() => {
+                    return res.status(404).send({ type: "Error", message: "Error while updating" });
+                  })
+              } else {
+                return res.status(404).send({ type: "Error", message: "Tokens does not match" });
+              }
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+            return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+          });
+      }
+    });
+  } else {
+    return res.status(403).send({ type: "Error", message: "Authorization token required" })
+  }
+};
+
+// Update username by current username
+exports.updatePassword = (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN, async (err) => {
+      if (err) {
+        return res.status(403).send({ type: "Error", message: "Authorization token required" })
+      } else {
+        await db.admins
+          .findOne({
+            where: {
+              Login: { [Op.eq]: req.params.oldusername },
+              Availiable: { [Op.eq]: true },
+            },
+          })
+          .then((getAdmin) => {
+            if (getAdmin == null) {
+              return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+            } else {
+              getAdmin.update({
+                Login: req.params.newusername
+              })
+                .then(() => {
+                  res.status(200).send({ type: "OK", message: "Successfully updated" })
+                })
+                .catch(() => {
+                  return res.status(404).send({ type: "Error", message: "Error while updating" });
+                })
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+            return res.status(404).send({ type: "Error", message: "Admin doesn't exists" });
+          });
       }
     });
   } else {
