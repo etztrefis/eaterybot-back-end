ac644feebe354ac3f6022e0753a8df5d550d0b05
etztrefis <etztrefishello@gmail.com>
2021-03-05 06:44:12 +0300

    :ok_man: added sender pararmetr for all the changes routes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/admins/", adminController.findAll);
-app.get("/api/admins/delete/:username/", adminController.delete);
+app.get("/api/admins/delete/:username/:sender", adminController.delete);
 app.get("/api/admins/:username/:password", adminController.findOne);
 app.get("/api/admins/availiable", adminController.findAllAvailiable);
 app.get("/api/admins/create/:username/:password/:code", adminController.create);
@@ -44,28 +44,32 @@ app.get("/api/codes/", codesController.findAllCodes);
 
 app.get("/api/products/", productsConrtoller.findAll);
 app.get("/api/products/less/", productsConrtoller.findAllLess);
-app.get("/api/products/delete/:id", productsConrtoller.delete);
-app.get("/api/products/create/:name/:amount/:units", productsConrtoller.create);
-app.get("/api/products/update/:id/:name/:amount/:units", productsConrtoller.update);
+app.get("/api/products/delete/:id/:sender", productsConrtoller.delete);
+app.get("/api/products/create/:name/:amount/:units/:sender", productsConrtoller.create);
+app.get("/api/products/update/:id/:name/:amount/:units/:sender", productsConrtoller.update);
 
 app.get("/api/dishes/", dishesController.findAll);
-app.get("/api/dishes/delete/:id", dishesController.delete);
-app.get("/api/dishes/create/:id/:name/:energy/:price", dishesController.create);
-app.get("/api/dishes/update/:id/:name/:energy/:price", dishesController.update);
+app.get("/api/dishes/delete/:id/:sender", dishesController.delete);
+app.get("/api/dishes/create/:id/:name/:energy/:price/:sender", dishesController.create);
+app.get("/api/dishes/update/:id/:name/:energy/:price/:sender", dishesController.update);
 
 app.get("/api/compositions/", compositionsController.findAll);
-app.get("/api/compositions/delete/:did/:pname", compositionsController.delete);
-app.get("/api/compositions/create/:did/:pname/:amount/", compositionsController.create);
-app.get("/api/compositions/update/:did/:pname/:amount/", compositionsController.update);
+app.get("/api/compositions/delete/:did/:pname/:sender", compositionsController.delete);
+app.get("/api/compositions/create/:did/:pname/:amount/:sender", compositionsController.create);
+app.get("/api/compositions/update/:did/:pname/:amount/:sender", compositionsController.update);
 
 app.get("/api/menu/", menuController.findAll);
-app.get("/api/menu/destroy", menuController.destroyAll);
+app.get("/api/menu/destroy/:sender", menuController.destroyAll);
 app.get("/api/menu/dishes", menuController.findAllProducts)
-app.get("/api/menu/delete/:dayofweek/:dishid", menuController.delete);
-app.get("/api/menu/create/:dayofweek/:dishid/", menuController.create);
+app.get("/api/menu/delete/:dayofweek/:dishid/:sender", menuController.delete);
+app.get("/api/menu/create/:dayofweek/:dishid/:sender", menuController.create);
 
 app.get("/api/orders/", ordersController.findAll);
-app.get("/api/orders/update/:id/:state", ordersController.update);
+app.get("/api/orders/update/:id/:state/:sender", ordersController.update);
+
+app.get("/api/stats/dishes/", statisticsController.getDishesStats);
+app.get("/api/stats/messages/", statisticsController.getUsersMessagesStats); 
+app.get("/api/stats/menu/", statisticsController.getMenuDishesStats);
 
 const db = require("./app/models");
 db.sequelize.sync().then(() => {
