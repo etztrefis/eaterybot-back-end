63e7504e765a123abbc9d5d6f15c11a0f42ce5e5
etztrefis <etztrefishello@gmail.com>
2021-04-28 18:35:14 +0300

    :gear: new controller for message sending

---
+++ app/controller/messages.controller.js
@@ -0,0 +1,20 @@
+process.env.NTBA_FIX_319 = 1;
+const TelegramBot = require('node-telegram-bot-api');
+require("dotenv").config({ path: "../../.env" });
+
+const telegramClient = new TelegramBot(process.env.TOKEN, { polling: true });
+
+// Send message to the telegram bot.
+exports.messageSend = async (req, res) => {
+	telegramClient.sendMessage('474782403', 
+`📨 New message from #eaterybot : 
+👋 Name: ${req.params.name || "No name provided"}
+📬 Emаil: ${req.params.email || "No email provided"}
+💬 Message: ${req.params.message || "No message provided"}`)
+		.then(() => {
+			return res.status(200).send({ type: "OK", message: "Sent." })
+		})
+		.catch(() => {
+			return res.status(200).send({ type: "Error", message: "Error while sending." })
+		});
+};
